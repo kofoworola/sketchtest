c56f2f11dfed9cc0ca991626be0153ae656d1d17
Kofo Okesola <okesolakofo@gmail.com>
2021-10-30 00:12:44 +0100

    server: formated server

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible // indirect
 	github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,46 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
 )
 
+type Config struct {
+	Port string `default:"8080"`
+}
+
 func main() {
+	// configuration
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/draw", drawHandler)
-	if err := http.ListenAndServe(":8085", nil); err != nil {
+	server := http.Server{
+		Addr:        ":" + cfg.Port,
+		ReadTimeout: time.Second * 10,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-done
+
+		log.Println("server shutting down...")
+		server.Shutdown(context.Background())
+	}()
+
+	log.Printf("starting server on port %s", cfg.Port)
+	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
 }
